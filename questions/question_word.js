a6ec351ed7f462388fa3ceefe49d33a03e30ade9
khanhnguyen2903 <nvkhanh084@gmail.com>
2024-07-18 19:31:01 +0700

    Add files via upload

--- questions/question_word.js
+++ questions/question_word.js
@@ -2,7 +2,7 @@
 const question_word = [
   { url_image: "images/duck.jpg", answer: "DUCK" },
   { url_image: "images/bear.jpg", answer: "BEAR" },
-  { url_image: "images/brid.jpg", answer: "BRID" },
+  { url_image: "images/bird.jpg", answer: "BIRD" },
   { url_image: "images/lion.jpg", answer: "LION" },
   { url_image: "images/monkey.png", answer: "MONKEY" },
 ];
